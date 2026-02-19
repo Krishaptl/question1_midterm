0f17c30d9c8dd211450508a9f0f2fa61d6d3e8fa
krish <krish@10.48.182.165>
2026-02-19 17:03:08 -0500

    Project setup by Krisha Patel

---
+++ src/inventorytracker/InventoryTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inventorytracker;
+
+/**
+ *
+ * @author krish
+ */
+public class InventoryTracker {
+
+    public static void main(String[] args) {
+        System.out.println("Inventory Tracker Started");
+    }
+}
