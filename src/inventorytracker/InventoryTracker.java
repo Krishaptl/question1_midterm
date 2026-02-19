720cf67a373b237fad02e37dde7fd0458a672e19
krish <krish@10.48.182.165>
2026-02-19 17:11:37 -0500

    Implemented stock checking by Krisha Patel

--- src/inventorytracker/InventoryTracker.java
+++ src/inventorytracker/InventoryTracker.java
@@ -13,4 +13,7 @@ public class InventoryTracker {
     public static void main(String[] args) {
         System.out.println("Inventory Tracker Started");
     }
+    public void checkStockLevel() {
+    System.out.println("Checking stock level...");
+}
 }
